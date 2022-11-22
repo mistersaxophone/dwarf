2648aa605e85553bdbc4e852747ee902258f11b3
Mister-Johnson-Fortnite <kjwar@DESKTOP-AJT7U7P>
2022-11-21 22:59:38 -0600

    i love not being able to work on my own schedule

--- src/main/java/net/mcreator/dwarffortressreal/procedures/CompressorUpdateTickProcedure.java
+++ src/main/java/net/mcreator/dwarffortressreal/procedures/CompressorUpdateTickProcedure.java
@@ -1,5 +1,7 @@
 package net.mcreator.dwarffortressreal.procedures;
 
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.items.IItemHandlerModifiable;
 import net.minecraftforge.items.CapabilityItemHandler;
 
 import net.minecraft.world.level.block.state.BlockState;
@@ -7,11 +9,17 @@ import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.LevelAccessor;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.item.Items;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.tags.ItemTags;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.core.BlockPos;
+
+import net.mcreator.dwarffortressreal.init.DwarfFortressRealModBlocks;
 
 import java.util.concurrent.atomic.AtomicReference;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.Random;
 
 public class CompressorUpdateTickProcedure {
 	public static void execute(LevelAccessor world, double x, double y, double z) {
@@ -66,7 +74,7 @@ public class CompressorUpdateTickProcedure {
 					_level.sendBlockUpdated(_bp, _bs, _bs, 3);
 			}
 		} else if (true) {
-			world.setBlock(new BlockPos(x, y, z), Blocks.ANDESITE_STAIRS.defaultBlockState(), 3);
+			world.setBlock(new BlockPos(x, y, z), DwarfFortressRealModBlocks.COMPRESSOR.get().defaultBlockState(), 3);
 		}
 		if (new Object() {
 			public double getValue(LevelAccessor world, BlockPos pos, String tag) {
@@ -84,7 +92,40 @@ public class CompressorUpdateTickProcedure {
 					return "";
 				}
 			}.getValue(world, new BlockPos(x, y, z), "itemBeingProduced")).equals("0")) {
-				world.setBlock(new BlockPos(x, y, z), Blocks.ANDESITE_STAIRS.defaultBlockState(), 3);
+				if ((new Object() {
+					public ItemStack getItemStack(LevelAccessor world, BlockPos pos, int slotid) {
+						AtomicReference<ItemStack> _retval = new AtomicReference<>(ItemStack.EMPTY);
+						BlockEntity _ent = world.getBlockEntity(pos);
+						if (_ent != null)
+							_ent.getCapability(CapabilityItemHandler.ITEM_HANDLER_CAPABILITY, null)
+									.ifPresent(capability -> _retval.set(capability.getStackInSlot(slotid).copy()));
+						return _retval.get();
+					}
+				}.getItemStack(world, new BlockPos(x, y, z), 1)).getItem() == Blocks.AIR.asItem()) {
+					{
+						BlockEntity _ent = world.getBlockEntity(new BlockPos(x, y, z));
+						if (_ent != null) {
+							final int _slotid = 1;
+							final ItemStack _setstack = new ItemStack((ForgeRegistries.ITEMS.tags()
+									.getTag(ItemTags.create(new ResourceLocation((("dwarf_fortress_real:" + (new Object() {
+										public String getValue(LevelAccessor world, BlockPos pos, String tag) {
+											BlockEntity blockEntity = world.getBlockEntity(pos);
+											if (blockEntity != null)
+												return blockEntity.getTileData().getString(tag);
+											return "";
+										}
+									}.getValue(world, new BlockPos(x, y, z), "itemBeingProduced")))).toLowerCase(java.util.Locale.ENGLISH))))
+									.getRandomElement(new Random()).orElseGet(() -> Items.AIR)));
+							_setstack.setCount(1);
+							_ent.getCapability(CapabilityItemHandler.ITEM_HANDLER_CAPABILITY, null).ifPresent(capability -> {
+								if (capability instanceof IItemHandlerModifiable)
+									((IItemHandlerModifiable) capability).setStackInSlot(_slotid, _setstack);
+							});
+						}
+					}
+				} else {
+					world.setBlock(new BlockPos(x, y, z), DwarfFortressRealModBlocks.COMPRESSOR.get().defaultBlockState(), 3);
+				}
 			}
 		}
 	}
